c6818f393263e15106813c3878705439857093d1
unknown <>
2016-06-28 17:28:51 +0800

    添加jQuery,并开始编写HTML5格式代码

--- js/subgroups.main.js
+++ js/subgroups.main.js
@@ -64,31 +64,5 @@ var test = (function(){
   // alert(bodyChilds.childNodes.length);
 })()
 
-/**
-*
-*移动滚动条动态调整导航
-*
-*/ 
-//var moveScrollAdjustHeader=(function(){var b=document.documentElement,g=document.querySelector(".header-main"),e=false,a=56;function f(){window.addEventListener("scroll",function(){if(!e){e=true;setTimeout(d,100)}},false);}function d(){var h=c();if(h>=a){classie.add(g,"header-main-shrink");}else{classie.remove(g,"header-main-shrink");}e=false;}function c(){return window.pageYOffset||b.scrollTop;}f();})();
 
-/**
-*
-*添加移动设备导航
-*去掉PC设备导航
-*移动设备优先
-*Mobile device priority
-*
-*/
-// var mobileDevicePriority=(function(){
-//   var m=document.querySelector(".m-header-home"),g=document.querySelector(".header-home"),t=document.querySelector(".tab-mobile-header"),s=document.getElementById("aboutus"),a=992;
-//   function d(){
-//     var w=r();
-//     if(w>a){classie.remove(g,"hidden");classie.remove(t,"blank-56");classie.remove(s,"hidden");classie.add(m,"hidden");classie.add(t,"blank-120");}
-//     else{}
-//   }
-//   function r(){
-//     return $(window).width();
-//   }
-//   d();
-// })()
 
